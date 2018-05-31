0c3b75e0b029ada0da42cfb5a19acc6205b90f8f
MIANBAO222 <756362171@qq.com>
2018-05-31 10:24:11 +0800

    更改

--- hardware_recommenddation/routes/index.js
+++ hardware_recommenddation/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
-const url = require("url");
+var url = require("url");
+var usingredis=require('../usingredis')
 var router = express.Router();
 var TCP=require('../connectWithTCP');
 var Cmysql=require('../connectToMysql');
 var app = express();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    return res.render('index', { title: 'Express' });
+    return res.render('control', { title: 'control' });
 });
 router.get('/login.html',function (req,res,next) {
 
@@ -22,6 +23,12 @@ router.get('/int',function (req,res,next) {
 })
 router.get('/control.html',function (req,res,next) {
     return res.render('control',{title:'control'});
+});
+router.get('/newspush.html',function (req,res,next) {
+    return res.render('newspush',{title:'newspush'});
+});
+router.get('/newspush2.html',function (req,res,next) {
+    return res.render('newspush2',{title:'newspush'});
 });
 router.post('/login',function (req,res,next) {
     console.log('req:', req.body);
@@ -43,7 +50,7 @@ router.post('/login',function (req,res,next) {
             return res.json({success:1,usertypes:rows[0].usertypes});
         }
         else {
-             res.json({success:0});
+             res.json(JSON.stringify({success:0}));
         }
     })
     // if(req.body.uname=="love" && req.body.upwd=="love") {
@@ -75,7 +82,7 @@ router.post('/register',function (req,res,next) {
         }
         else {
             Cmysql.init();
-            Cmysql.connection.query('insert into log(uname,pwd) VALUES(?,?)',[req.body.uname,req.body.upwd],function (err, rows, fields) {
+            Cmysql.connection.query('insert into log(uname,pwd,usertypes) VALUES(?,?,?)',[req.body.uname,req.body.upwd,'0'],function (err, rows, fields) {
                 if (err) {
                     console.log(err);
                     return res.json({success:0});
@@ -90,8 +97,18 @@ router.post('/register',function (req,res,next) {
 });
 
 router.post('/cal',function (req,res,next) {
-    console.log('req:',req.body);
     var str=req.body;
+    str.sessionid=req.session.id;
+    var string = JSON.stringify(str)
+    usingredis.push_message(string);
+    return res.json({success:1});
+
+//弃用 原tcp连接方式
+    var str=req.body;
+    str.sessionid=req.session.id;
+    var string = JSON.stringify(str)
+    usingredis.push_message(string);
+    console.log('req:',str);
     TCP.datainit(str);
     TCP.TCP_connect_start();
     TCP.status.on('success',function (data) {
@@ -112,9 +129,38 @@ router.post('/cal',function (req,res,next) {
             console.log(e);
         }
     })
-
-    // res.json({rom:"1TB HDD",cpu2:"Intel Xeon X5670 @ 2.93GHz",score2:"8956",tmb:"主板+LGA1366",lowest:"0",ram:"16GB",gcard2:"GeForce GT 730",price2:"2348.3557"});
 })
+    router.get('/calresult',function (req,res,next) {
+        Cmysql.init();
+        Cmysql.connection.query('select resultjson from result where session =? ',[req.session.id],function (err, rows, fields) {
+            console.log(rows.lenth);
+            if (err) {
+                console.log(err);
+                return res.json({success:0});
+            }
+            else if(rows[0]!=null){//找到了结果
+                console.log(rows[0]);
+                var temp=JSON.parse(rows[0].resultjson);
+                console.log(temp);
+                Cmysql.connection.query('delete from result where session =?',[req.session.id],function (err, rows, fields) {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+                return res.json(temp);
+                // var user = {'username': req.body.uname};
+                // req.session.user = true;
+                // res.json({success:1});
+            }
+            else {
+                console.log(rows[0]);
+                return res.json({wait:1});
+            }
+        })
+       // return res.render('newspush',{title:'newspush'});
+    });
+    // res.json({rom:"1TB HDD",cpu2:"Intel Xeon X5670 @ 2.93GHz",score2:"8956",tmb:"主板+LGA1366",lowest:"0",ram:"16GB",gcard2:"GeForce GT 730",price2:"2348.3557"});
+
 router.get('/getNews',function (req,res,next) {
     var url_n=req.originalUrl;
     console.log(url_n);
@@ -156,7 +202,7 @@ router.get('/getNews',function (req,res,next) {
     else if(obj.flush==1){
         var time=req.headers.cookietime;
         Cmysql.init();
-        Cmysql.connection.query('select * from news where time>? order by time DESC limit 0,5',[time], function(err, rows, fields) {
+        Cmysql.connection.query('select * from news where time>? order by time DESC limit 0,4',[time], function(err, rows, fields) {
             if (err) {
                 console.log(err);
                 res.json({success:0});
@@ -177,5 +223,34 @@ router.get('/getNews',function (req,res,next) {
             console.log('connection_end: ');
         });
     }
+})
+
+router.post('/setNews',function (req,res,next) {
+    console.log('req:', req.body);
+    Cmysql.init();
+    Cmysql.connection.query('select title from news where title=?',[req.body.title],function (err, rows, fields) {
+        //console.log(rows.lenth);
+        if (err) {
+            console.log(err);
+            return res.json({success:0});
+        }
+        else if(rows[0]!=null){//相同title
+            console.log(rows);
+            return res.json({success:0,same:1});
+        }
+        else {
+            Cmysql.init();
+            Cmysql.connection.query('insert into news(image,title,keywords,text) VALUES(?,?,?,?)',[req.body.image,req.body.title,req.body.keywords,req.body.text],function (err, rows, fields) {
+                if (err) {
+                    console.log(err);
+                    return res.json({success:0});
+                }
+                else {
+                    return res.json({success:1});
+                    //return res.redirect("/newspush.html")
+                }
+            })
+        }
+    })
 })
 module.exports = router;
